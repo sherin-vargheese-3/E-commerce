cc439bb342aa099e10f7c83c6ffa8a1ceae8bf5b
sherin-vargheese-3 <sherin.v@edstem.com>
2026-01-05 08:46:47 +0530

    added repository annotation

--- src/main/java/com/example/ecommerce/repository/CustomerRepository.java
+++ src/main/java/com/example/ecommerce/repository/CustomerRepository.java
@@ -2,7 +2,9 @@ package com.example.ecommerce.repository;
 
 import com.example.ecommerce.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
     Customer findByCustomerNumber(String customerNumber);
 }
